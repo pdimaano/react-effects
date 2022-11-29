ecc3ad1f1aef618d125376ad5ed4ea4329abbc46
Philip Dimaano <pdimaano7@gmail.com>
2022-11-29 12:38:53 -0800

    Initial working solution with drawing cards from deck

--- src/Deck.js
+++ src/Deck.js
@@ -1,22 +1,44 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 
-const BASE_URL = 'https://deckofcardsapi.com/api/deck/'
+const BASE_URL = 'https://deckofcardsapi.com/api/deck'
 
 function Deck() {
   const [deck, setDeck] = useState({deck_id: ""})
+  const [card, setCard] = useState({code: "", image: ""})
+  const [empty, setEmpty] = useState(false)
+  console.log("Deck", deck, card)
 
   useEffect(function fetchDeckOnMount() {
     async function fetchDeck() {
       const deckResult = await axios.get(`${BASE_URL}/new/shuffle`);
-      setDeck({deck_id: deckResult.deck_id});
+      console.log(deckResult)
+      setDeck({deck_id: deckResult.data.deck_id});
     }
     fetchDeck();
   }, []);
 
+  async function drawCard() {
+    try{
+      const cardResult = await axios.get(`${BASE_URL}/${deck.deck_id}/draw`);
+      console.log(cardResult);
+      setCard({
+        code: cardResult.data.cards[0].code,
+        image: cardResult.data.cards[0].image,
+      })
+    } catch(error) {
+      console.log(error)
+      setEmpty(true)
+    }
+  }
+
   return (
     <div className="Deck">
-
+      <button onClick={(drawCard)}>
+        Draw Card!
+      </button>
+      <img src={card.image} alt={card.code} />
+      {empty ? <p>Error: No cards remaining!</p> : null}
     </div>
   );
 }
